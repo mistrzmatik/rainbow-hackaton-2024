2027cbcc28ced3a87c151afb76da6157b7605f33
Mateusz Kraska <mistrzmatik@gmail.com>
2024-10-03 23:12:41 +0200

    proto init

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use point_salad::game_client::GameClient;
+
+pub mod point_salad {
+    tonic::include_proto!("point_salad");
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut client = GameClient::connect("http://[::1]:10000").await?;
+
+//     // let request = tonic::Request::new(HelloRequest {
+//     //     name: "Tonic".into(),
+//     // });
+
+//     // let response = client.say_hello(request).await?;
+
+//     // println!("RESPONSE={:?}", response);
+
+    Ok(())
 }
