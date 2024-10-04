ffa27935ba408aac248c24ec577bc83bef429bfb
Michał Ślifierski <m.slifierski@ufin.pl>
2024-10-04 12:46:41 +0200

    czytanie kart

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,29 @@
-use point_salad::game_client::GameClient;
+use std::{fs, io::Read};
 
-pub mod point_salad {
-    tonic::include_proto!("point_salad");
+// use point_salad::{game_client::GameClient, Card};
+
+
+mod models;
+
+fn load_all_cards(file_name: String) -> Vec<models::Card> {
+    let mut file = fs::File::open(file_name).expect("Unable to open file");
+    let mut json_data = String::new();
+    file.read_to_string(&mut json_data).expect("Unable to read file");
+
+    let cards: Vec<models::Card> = serde_json::from_str(&json_data)
+        .expect("JSON was not well-formatted");
+
+    cards
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mut client = GameClient::connect("http://[::1]:10000").await?;
+    let vegetables = models::Vegetable::list();
+    let cards = load_all_cards("data.json".to_string());
 
-//     // let request = tonic::Request::new(HelloRequest {
-//     //     name: "Tonic".into(),
-//     // });
-
-//     // let response = client.say_hello(request).await?;
-
-//     // println!("RESPONSE={:?}", response);
+    for card in cards {
+        println!("{:?}", card);
+    }
 
     Ok(())
 }
