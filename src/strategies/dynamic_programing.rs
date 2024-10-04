5a97c698e98a961872149509332b2d9b5e2dfcf5
Michał Ślifierski <m.slifierski@ufin.pl>
2024-10-04 15:28:28 +0200

    dp init

--- src/strategies/dynamic_programing.rs
+++ src/strategies/dynamic_programing.rs
@@ -1,4 +1,4 @@
-use crate::point_salad_server::GameState;
+use crate::point_salad_server::{GameState, Hand};
 use crate::strategies::strategy::Strategy;
 
 pub struct DynamicProgramingStrategy {}
@@ -7,11 +7,32 @@ impl DynamicProgramingStrategy {
     pub fn new() -> DynamicProgramingStrategy {
         DynamicProgramingStrategy {}
     }
+
+    fn unwrap_hand(hand: &Option<Hand>) -> Hand {
+        hand
+            .clone()
+            .unwrap_or(Hand {
+                point_cards: vec![],
+                vegetables: vec![],
+            })
+    }
+
+    fn my_hand(&self, state: &GameState) -> Hand {
+        DynamicProgramingStrategy::unwrap_hand(&state.your_hand.clone())
+    }
+
+    fn oponent_hand(&self, state: &GameState) -> Hand {
+        DynamicProgramingStrategy::unwrap_hand(&state.opponents_hands.get(0).cloned())
+    }
 }
 
 impl Strategy for DynamicProgramingStrategy {
     fn make_take_cards_move(&self, state: &GameState) -> Vec<String> {
-        todo!()
+        let my_hand = self.my_hand(state);
+        let oponent_hand: Hand = self.oponent_hand(state);
+
+
+        vec![]
     }
 
     fn make_flip_move(&self, state: &GameState) -> Vec<String> {
