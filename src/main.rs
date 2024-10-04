4e392f97a6886755acd44a0331c45d90473c7263
Michał Ślifierski <m.slifierski@ufin.pl>
2024-10-04 18:05:08 +0200

    helpers added

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,7 @@ use crate::strategies::strategy::Strategy;
 mod point_salad_server;
 mod strategies;
 mod models;
+mod helpers;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
